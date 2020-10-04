787dae253b05a20b0da6d270e17b3dfc1181994c
Devarshi Singh <52333689+singhdevarshi@users.noreply.github.com>
2020-10-04 17:36:23 +0530

    Update app.php
    add app url

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'singhdevarshi.github.io'),
+    'url' => env('APP_URL', 'https://singhdevarshi.github.io/'),
 
     'asset_url' => env('ASSET_URL', null),
 
