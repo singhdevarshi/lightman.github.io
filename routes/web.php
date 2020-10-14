aaf0b6c0d5eb3453176e93606640dede0ba57ba2
Devarshi Singh <52333689+singhdevarshi@users.noreply.github.com>
2020-10-14 16:27:48 +0530

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', 'FrontendController@index');
 
-Route::get('firebase','FirebaseController@index');
 
 Route::get('/about-us', 'FrontendController@aboutus');
 Route::get('/services', 'FrontendController@services');
